932f2cdaa09ed065a6dcc8041054caa2aa638bbc
Victor Bezerra <victorbezerr@hotmail.com>
2018-08-04 17:05:14 -0300

    views cadastrar

--- routes/cadastrar.js
+++ routes/cadastrar.js
@@ -1,14 +1,90 @@
 const express = require('express');
 const router = express.Router();
+const marca = require('./../model/marca.js')
 
 router.get('/', (req,res)=>{
-  res.render('marca',{
-    message: 'TESTE'
+  res.render('listaCadastros',{
+    message: 'Lista de cadastros'
   });
 });
 
-router.post('/marca', (req,res)=>{
-  res.render('listaMarca',req.body)
+router.get('/marca', (req,res)=>{
+  res.render('cadastrarMarca',{
+    message: 'Cadastrar Marca'
+  });
 });
+
+router.get('/carroceria', (req,res)=>{
+  res.render('cadastrarCarroceria',{
+    message: 'Cadastrar Carroceria'
+  });
+});
+
+router.get('/genero', (req,res)=>{
+  res.render('cadastrarGenero',{
+    message: 'Cadastrar Genero'
+  });
+});
+
+router.get('/tipo', (req,res)=>{
+  res.render('cadastrarTipo',{
+    message: 'Cadastrar Tipo'
+  });
+});
+
+router.get('/especie', (req,res)=>{
+  res.render('cadastrarEspecie',{
+    message: 'Cadastrar Especie'
+  });
+});
+
+router.get('/modelo', (req,res)=>{
+  res.render('cadastrarModelo',{
+    message: 'Cadastrar Modelo'
+  });
+});
+
+router.get('/combustivel', (req,res)=>{
+  res.render('cadastrarCombustivel',{
+    message: 'Cadastrar Combustivel'
+  });
+});
+
+router.get('/status', (req,res)=>{
+  res.render('cadastrarStatus',{
+    message: 'Cadastrar Status'
+  });
+});
+
+router.get('/propriedade', (req,res)=>{
+  res.render('cadastrarPropriedade',{
+    message: 'Cadastrar Propriedade'
+  });
+});
+
+router.get('/cor', (req,res)=>{
+  res.render('cadastrarCor',{
+    message: 'Cadastrar Cor'
+  });
+});
+
+router.get('/veiculo', (req,res)=>{
+  res.render('cadastrarVeiculo',{
+    message: 'Cadastrar Veiculo'
+  });
+});
+
+router.get('/motorista', (req,res)=>{
+  res.render('cadastrarMotorista',{
+    message: 'Cadastrar Motorista'
+  });
+});
+
+
+// router.get('/marca', (req,res)=>{
+//   res.render('listaMarca',{
+//     message: 'Teste'
+//   })
+// });
 
 module.exports = router;
