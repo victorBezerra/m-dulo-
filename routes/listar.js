5f6deb22d30037368b49f7ca19ae12c3c32303ff
Victor Bezerra <victorbezerr@hotmail.com>
2018-08-07 11:17:01 -0300

    lista motorista OK

--- routes/listar.js
+++ routes/listar.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const motorista = require('./../model/motorista.js');
 
 router.get('/',(req,res)=>{
   res.render('listas');
+});
+
+router.get('/motoristas',(req,res)=>{
+  motorista.findAll()
+    .then((result)=>{
+      var motoristas = result;
+      res.render('listaMotorista',{
+        message: 'Lista de motorista',
+        motoristas
+      })
+    })
+    .catch((err)=>{
+      console.log("ERRO: ",err);
+    });
 });
 
 module.exports = router;
