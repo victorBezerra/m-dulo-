9ec8bb16727df90cfa219f5643d99657c650c9ae
Victor Bezerra <victorbezerr@hotmail.com>
2018-08-08 15:39:44 -0300

    editar motorista e veiculos OK

--- routes/excluir.js
+++ routes/excluir.js
@@ -1,4 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const motorista = require('./../model/motorista');
+const veiculo = require('./../model/veiculo');
+
+router.get('/motorista/:cpf',(req,res)=>{
+  motorista.destroy({
+    where:{
+      cpf: req.params.cpf
+    }
+  })
+    .then(()=>{
+      res.redirect('/listar');
+    })
+    .catch((err)=>{
+      console.log("ERRO: ",err);
+    });
+});
+
+router.get('/veiculo/:id',(req,res)=>{
+  veiculo.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+    .then(()=>{
+      res.redirect('/listar');
+    })
+    .catch((err)=>{
+      console.log("ERRO: ",err);
+    });
+});
 
 module.exports = router;
