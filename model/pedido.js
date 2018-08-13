ca56594b702b0ff5e68b9ce78b82d817f2e83156
Victor Bezerra <victorbezerr@hotmail.com>
2018-08-13 10:47:54 -0300

    fazendo setor, usuario e pedido

---
+++ model/pedido.js
@@ -0,0 +1,64 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("mysql://root@localhost:3306/transporte");
+
+const Pedido = sequelize.define("pedidos",{
+  tipoTransporte:{
+    type: Sequelize.BIGINT
+  },
+  qtdPass:{
+    type: Sequelize.INTEGER
+  }
+  dataPedido:{
+      type. Sequelize.DATE
+  },
+  horaPedido:{
+    type: Sequelize.TIME
+  }
+  solicitante:{
+    type: Sequelize.INTEGER
+  },
+  setorSolicitante:{
+    type: Sequelize.INTEGER
+  }
+  localPartida:{
+    type: Sequelize.TEXT
+  },
+  localDestino:{
+    type: Sequelize.TEXT
+  },
+  localRetorno:{
+    type: Sequelize.TEXT
+  },
+  viagemAer:{
+    type: Sequelize.STRING
+  },
+  dataPartida:{
+    type: Sequelize.DATE
+  },
+  horaPartida:{
+    type: Sequelize.TIME
+  }
+  dataChegada:{
+    type: Sequelize.DATE
+  },
+  horaChegada:{
+    type: Sequelize.TIME
+  },
+  proposto:{
+    type: Sequelize.TEXT
+  },
+  telefone:{
+    type: Sequelize.STRING
+  }
+  email:{
+    type: Sequelize.STRING
+  },
+  obj:{
+    type: Sequelize.TEXT
+  },
+  finalidade:{
+    type: Sequelize.TEXT
+  }
+});
+
+module.exports = Pedido;
---
+++ model/usuario.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("mysql://root@localhost:3306/transporte");
+
+const Usuario = sequelize.define("usuarios",{
+  id:{
+    type: Sequelize.BIGINT,
+    primaryKey:true,
+    autoIncrement:true
+  },
+  nome:{
+    type: Sequelize.TEXT
+  }
+  cpf:{
+    type: Sequelize.INTEGER
+  }
+  funcao:{
+    type: Sequelize.TEXT
+  }
+  senha:{
+    type: Sequelize.TEXT
+  }
+  email:{
+    type: Sequelize.TEXT
+  }
+  cpf:{
+    type: Sequelize.TEXT
+  }
+  status:{
+    type: Sequelize.TEXT
+  },
+  {
+    timestamps:false,
+    freezeTableName:true
+});
+
+module.exports = Usuario;
